396d51ba9f18c7e64ae0cc5f87a1bb487a73c978
cleofan <58447038+cleofan@users.noreply.github.com>
2022-04-24 14:26:21 -0400

    Update send_tokens.py

--- send_tokens.py
+++ send_tokens.py
@@ -18,7 +18,8 @@ def connect_to_algo(connection_type=''):
         # TODO: return an instance of the client for sending transactions
         # Tutorial Link: https://developer.algorand.org/tutorials/creating-python-transaction-purestake-api/
         algod_address = "https://testnet-algorand.api.purestake.io/ps2"
-        return algodclient = algod.AlgodClient(algod_token, algod_address, headers=purestake_token)
+        algodclient = algod.AlgodClient(algod_token, algod_address, headers=purestake_token)
+        return algoclient
 
     return None
 
