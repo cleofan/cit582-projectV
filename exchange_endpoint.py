d0235dbf11575ba700e7ab7b61e6a26043d60d19
cleofan <58447038+cleofan@users.noreply.github.com>
2022-04-25 18:34:53 -0400

    Update exchange_endpoint.py

--- exchange_endpoint.py
+++ exchange_endpoint.py
@@ -137,7 +137,7 @@ def fill_order(order, txes=[]):
     # Validate the order has a payment to back it (make sure the counterparty also made a payment)
     # Make sure that you end up executing all resulting transactions!
     
-    query = g.session.query(Order).filter(Order.filled == None, Order.buy_currency == order.sell_currency, Order.sell_currency == order.buy_currency, Order.sell_amount / Order.buy_amount >= order.buy_amount/order.sell_amount)   
+    query = g.session.query(Order).filter(!Order.tx_id is None, Order.filled == None, Order.buy_currency == order.sell_currency, Order.sell_currency == order.buy_currency, Order.sell_amount / Order.buy_amount >= order.buy_amount/order.sell_amount)   
     count = query.count()
     if count > 0:
         existing_order = query.first()
